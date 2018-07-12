d4959eaf3d10ab4fc51750059da31f86d702ced9
MAYUR MAHAJAN <mayur.mahajan@icloud.com>
2018-07-11 23:36:29 -0700

    Update nio version

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Bouke/DNS.git", .revision("2d13515ea1f697170c9c32f7708ac508e749bf50")),
 //        .package(url: "https://github.com/apple/swift-nio.git", from: "1.0.0"),
-        .package(url: "https://github.com/mayur-mahajan/swift-nio.git", .revision("2e2cc6029b9fc2a00530db9c57c5f55f169fb668")),
+        .package(url: "https://github.com/mayur-mahajan/swift-nio.git", .revision("d913c886befe2f92ae250ad1666131b6013e32d9")),
         .package(url: "https://github.com/IBM-Swift/BlueSocket.git", from: "1.0.0")
     ],
     targets: [
